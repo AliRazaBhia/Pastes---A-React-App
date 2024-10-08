2825b834ee401c3f6e38f9105ef0c40b1e58ec18
ali raza <mailedinnewmail@gmail.com>
2024-10-08 20:42:33 +0500

    with logo

--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -4,6 +4,9 @@ import { Link, NavLink } from 'react-router-dom'
 const Navi = () => {
   return (
     <>
+    <div className='mt-15'>
+      <span className='font-black text-blue-400 text-5xl'><span className='text-blue-300'>PASTES </span> - A react App</span>
+    </div>
     <div className='w-[100%] flex content-center justify-center '>
         <div className='flex font-bold min-w-[50%] flex-row gap-3 bg-white text-blue-500 justify-center mt-10'>
        <NavLink className={ ({isActive}) => isActive ? "text-indigo-500 drop-shadow-sm  transition ease-in-out duration-150" : "text-blue-500"} to="/" >Home</NavLink>
